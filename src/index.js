510f8ed49d9654ad10fa9dbc8dec4d4444e386df
Denys Bilonozhko <deformix@gmail.com>
2021-07-04 16:10:18 +0300

    refactor: no parameter reassign

--- src/index.js
+++ src/index.js
@@ -1,67 +1,75 @@
 module.exports = function toReadable(number) {
     const digits = {
-        '0': 'zero',
-        '1': 'one',
-        '2': 'two',
-        '3': 'three',
-        '4': 'four',
-        '5': 'five',
-        '6': 'six',
-        '7': 'seven',
-        '8': 'eight',
-        '9': 'nine',
-        '10': 'ten',
-        '11': 'eleven',
-        '12': 'twelve',
-        '13': 'thirteen',
-        '14': 'fourteen',
-        '15': 'fifteen',
-        '16': 'sixteen',
-        '17': 'seventeen',
-        '18': 'eighteen',
-        '19': 'nineteen',
-        '20': 'twenty',
-        '30': 'thirty',
-        '40': 'forty',
-        '50': 'fifty',
-        '60': 'sixty',
-        '70': 'seventy',
-        '80': 'eighty',
-        '90': 'ninety',
-        '100': 'hundred'
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+        100: "hundred",
     };
 
     if (number <= 20) {
         return digits[number];
     }
 
-    if (number <= 99) {
-        number = number.toString();
+    const numberString = number.toString();
 
-        if (number[1] > 0) {
-            return `${digits[number[0] + '0']} ${digits[number[1]]}`;
+    if (number <= 99) {
+        if (numberString[1] > 0) {
+            return `${digits[numberString[0] + "0"]} ${
+                digits[numberString[1]]
+            }`;
         }
 
         return `${digits[number]}`;
     }
 
     if (number >= 100) {
-        number = number.toString();
+        if (numberString[1] == 0 && numberString[2] == 0) {
+            return `${digits[numberString[0]]} ${digits["100"]}`;
+        }
 
-        if (number[1] == 0 && number[2] == 0) {
-            return `${digits[number[0]]} ${digits['100']}`;
-        } 
+        if (numberString[1] == 0 && numberString[2] != 0) {
+            return `${digits[numberString[0]]} ${digits["100"]} ${
+                digits[numberString[2]]
+            }`;
+        }
 
-        if (number[1] == 0 && number[2] != 0) {
-            return `${digits[number[0]]} ${digits['100']} ${digits[number[2]]}`;
-        } 
-
-        if (number.slice(1, 3) <= 20) {
-            return `${digits[number[0]]} ${digits['100']} ${digits[number.slice(1, 3)]}`;
-        } else if (number.slice(1, 3) <= 99 && number[2] == 0) {
-            return `${digits[number[0]]} ${digits['100']} ${digits[number[1] + '0']}`;
+        if (numberString.slice(1, 3) <= 20) {
+            return `${digits[numberString[0]]} ${digits["100"]} ${
+                digits[numberString.slice(1, 3)]
+            }`;
+        } else if (numberString.slice(1, 3) <= 99 && numberString[2] == 0) {
+            return `${digits[numberString[0]]} ${digits["100"]} ${
+                digits[numberString[1] + "0"]
+            }`;
         } else {
-            return `${digits[number[0]]} ${digits['100']} ${digits[number[1] + '0']} ${digits[number[2]]}`;
-        }     
-  }  
+            return `${digits[numberString[0]]} ${digits["100"]} ${
+                digits[numberString[1] + "0"]
+            } ${digits[numberString[2]]}`;
+        }
+    }
 };
